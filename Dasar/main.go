6275a18f69be51ec47b85d4c67619e2daeca72c3
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-05 18:12:46 +0700

    add operator -> dasar

--- Dasar/main.go
+++ Dasar/main.go
@@ -6,15 +6,30 @@ import (
 
 func main() {
 	tipeData()
+	operator()
 }
 
 func tipeData() {
 	fmt.Println("Hello Men") //Strings
-	
+
 	fmt.Println(12)  //int
 	fmt.Println(1.2) //float
-	
+
 	//booleans
 	fmt.Println(true)
 	fmt.Println(false)
+}
+
+func operator() {
+	// math
+	fmt.Println("operasi matematika : ")
+	fmt.Println(1 + 1)
+	fmt.Println(1 - 1)
+	fmt.Println(1 * 1)
+	fmt.Println(1 / 1)
+	// logic
+	fmt.Println("operasi logika : ")
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
 }
