ca0752e400ddd14edcdf651b738602775329dca2
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-05 18:08:17 +0700

    dasar and data types

---
+++ Dasar/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	tipeData()
+}
+
+func tipeData() {
+	fmt.Println("Hello Men") //Strings
+	
+	fmt.Println(12)  //int
+	fmt.Println(1.2) //float
+	
+	//booleans
+	fmt.Println(true)
+	fmt.Println(false)
+}
